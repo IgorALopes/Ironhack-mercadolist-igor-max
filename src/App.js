b62f49c9a18e38cba7eb8fa79ca022533b41d5b5
Maxwell Paulo <Maxwell@Maxwells-MacBook-Pro.local>
2022-09-09 16:45:34 -0300

    delete with error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import { AddItem } from "./pages/AddItem";
-import {Home} from "./pages/Home"
+import { Home } from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <>  
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/add-item' element={<AddItem />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/add-item" element={<AddItem />} />
       </Routes>
     </>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,68 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
-import{Link} from "react-router-dom"
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 export function Home() {
-    const [produtos, setProdutos] = useState([])
+  const [produtos, setProdutos] = useState([]);
+  const [needReload, setNeedReload] = useState(false);
 
-    useEffect(() => {
-        async function fetchProdutos() {
-            try{
-                const response = await axios.get(
-                    "https://ironrest.herokuapp.com/mercadoIgorMax"
-                )
-                
-                setProdutos([...response.data])
+  useEffect(() => {
+    async function fetchProdutos() {
+      try {
+        const response = await axios.get(
+          "https://ironrest.herokuapp.com/mercadoIgorMax"
+        );
 
-            } catch(err){
-                console.log(err)
-            }
-        }
-        fetchProdutos()
-    }, [])
-    
-    return (
-        <>
-            <h1>Lista de Compras</h1>
-            <Link to="/add-item">
-            <button>Adicionar item</button>
-            </Link>
-            {produtos.map((currentProduct) => {
-                return <h2>{currentProduct.product}</h2>
-            })}
-        </>
-    )
+        setProdutos([...response.data]);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchProdutos();
+  }, [needReload]);
+
+  function handleToast(id) {
+    toast((t) => (
+      <span>
+        Tem certeza que quer <b>deletar</b>?
+        <button onClick={handleDelete(id)}>Sim</button>
+        <button onClick={() => toast.dismiss(t.id)}>Não</button>
+      </span>
+    ));
+  }
+
+  async function handleDelete(id) {
+    try {
+      console.log(id);
+      await axios.delete(`https://ironrest.herokuapp.com/mercadoIgorMax/${id}`);
+      setNeedReload(true);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <>
+      <h1>Lista de Compras</h1>
+      <Link to="/add-item">
+        <button>Adicionar item</button>
+      </Link>
+      {produtos.map((currentProduct) => {
+        return (
+          <>
+            <h2>{currentProduct.product}</h2>
+
+            <button
+              onClick={() => {
+                handleToast(currentProduct._id);
+              }}
+            >
+              Deletar
+            </button>
+          </>
+        );
+      })}
+    </>
+  );
 }
